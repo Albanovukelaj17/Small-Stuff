333898ace322c12fb3f530bd55ec6adb0f788943
Albano <albigoal@outlook.com>
2024-08-27 12:15:07 +0200

    sys

---
+++ tttsimple/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Tic-Tac-Toe!");
+        System.out.println("This is a two-player game where Player X and Player O take turns.");
+        System.out.println("The goal is to get three of your marks in a row, either horizontally, vertically, or diagonally.");
+        System.out.println("Players will enter the row and column where they want to place their mark.");
+        System.out.println("The game ends when there is a winner or the board is full, resulting in a tie.");
+        System.out.println("Let's start the game!");
+
+
+        for (int i = 1; i <= 5; i++) {
+
+
+            System.out.println("i = " + i);
+        }
+    }
+}
